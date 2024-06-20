148566345228716c52c35df38b01fe40fd946c1c
Stanislav Mihaylov <stan@zenrocklabs.io>
2024-06-20 22:49:45 +0300

    Add prod API url

--- packages/snap/src/config.ts
+++ packages/snap/src/config.ts
@@ -1,5 +1,4 @@
-// export const qredoApiUrl = 'https://api-v2.qredo.network/api/v2';
-export const qredoApiUrl = 'http://localhost:8003/api/v2';
+export const qredoApiUrl = 'https://api-v2.qredo.network/api/v2';
 
 export const allowedRPCOrigins: Set<string> = new Set([
   'https://qredo.network',
